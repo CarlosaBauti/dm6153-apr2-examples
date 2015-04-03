4bf039aa6e6d07c89373b7c4104e156931782b38
Ramsey Nasser <ram@nas.sr>
2015-04-03 11:57:19 -0400

    Initial commit

---
+++ Assets/BoardPosition.js
@@ -0,0 +1,7 @@
+﻿var x : int;
+var y : int;
+
+function Update () {
+  transform.position.x = 10 * x + 5;
+  transform.position.z = 10 * y + 5;
+}
---
+++ Assets/MoveWithKeyboard.js
@@ -0,0 +1,4 @@
+﻿function Update () {
+  transform.position.x += Input.GetAxis("Horizontal");
+  transform.position.y += Input.GetAxis("Vertical");
+}
